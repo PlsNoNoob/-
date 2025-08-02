1d3c7241f9cedf801b11d9e482b2c7cbf199cef6
PlsNoNoob <jasperbns4@gmail.com>
2025-08-02 18:54:34 +0900

    Update GagNotOp.lua

--- GagNotOp.lua
+++ GagNotOp.lua
@@ -1,4 +1,4 @@
 -- For Mobile Copy:
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/3x67ehV43dPpepmh/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/KljcVHpkU7u2p0RT/raw", true))()
 wait(1)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/PlsNoNoob/-/refs/heads/main/TradeGag.lua'))()
