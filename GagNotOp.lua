5fbcb9e31e17c761929a9c798beb83a7885db348
PlsNoNoob <jasperbns4@gmail.com>
2025-08-20 23:08:51 +0900

    Update GagNotOp.lua

--- GagNotOp.lua
+++ GagNotOp.lua
@@ -1,4 +1,1 @@
-ID="79caa49f-13da-4216-bba2-92f31fa51cb2";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
-wait(1)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/PlsNoNoob/-/refs/heads/main/TradeGag.lua'))()
